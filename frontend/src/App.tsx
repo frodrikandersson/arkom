e481a357004b2b04fa6dc0171df90f55b4b5700a
frodrikandersson <frodrikandersson@gmail.com>
2025-12-11 22:01:49 +0100

    loading fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,30 @@ import { BrowserRouter } from 'react-router-dom';
 import { StackProvider, StackTheme, useUser } from '@stackframe/react';
 import { stackClientApp } from './config/stack';
 import { AuthProvider } from './contexts/AuthContext';
-import { ThemeProvider } from './contexts/ThemeContext';
+import { ThemeProvider, useTheme } from './contexts/ThemeContext';
 import { AppRoutes } from './routes/AppRoutes';
 
 function AppContent() {
+  const { isLoading } = useTheme();
+  
+  // Don't render until theme is loaded to prevent flash of unstyled content
+  if (isLoading) {
+    return null;
+  }
+  
+  return (
+    <AuthProvider>
+      <AppRoutes />
+    </AuthProvider>
+  );
+}
+
+function ThemedApp() {
   const user = useUser();
   
   return (
     <ThemeProvider userId={user?.id}>
-      <AuthProvider>
-        <AppRoutes />
-      </AuthProvider>
+      <AppContent />
     </ThemeProvider>
   );
 }
@@ -24,7 +37,7 @@ export const App: React.FC = () => {
       <BrowserRouter>
         <StackProvider app={stackClientApp}>
           <StackTheme>
-            <AppContent />
+            <ThemedApp />
           </StackTheme>
         </StackProvider>
       </BrowserRouter>
