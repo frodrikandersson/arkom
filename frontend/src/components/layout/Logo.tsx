8f8a76ac4cdce96c65ea2cbfd10d1154a0de3843
frodrikandersson <frodrikandersson@gmail.com>
2026-01-08 21:41:03 +0100

    last last css changes for lighthouse points

--- frontend/src/components/layout/Logo.tsx
+++ frontend/src/components/layout/Logo.tsx
@@ -3,8 +3,9 @@ import styles from './Logo.module.css';
 
 export const Logo = () => {
   return (
-    <Link to="/" className={styles.logo}>
-      <svg width="120" height="32" viewBox="0 0 120 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <Link to="/" className={styles.logo} aria-label="Arkom Home">
+      <svg width="120" height="32" viewBox="0 0 120 32" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Arkom Logo">
+        <title>Arkom</title>
         <path d="M8 24L14 8L20 24M10 19H18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
         <path d="M28 24V8H34C36 8 38 10 38 12C38 14 36 16 34 16H28M34 16L38 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
         <path d="M46 24V8M46 16L54 8M46 16L54 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
